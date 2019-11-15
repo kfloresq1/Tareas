1a47ec765d90e8a18c14f3063a3aea499a6f75a9
kfloresq1 <56986608+kfloresq1@users.noreply.github.com>
2019-11-14 19:36:00 -0500

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,39 @@
+Python 3.7.2 (tags/v3.7.2:9a3ffc0492, Dec 23 2018, 22:20:52) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> type(98)
+<class 'int'>
+>>> type(98.6)
+<class 'float'>
+>>> type("Hi")
+<class 'str'>
+>>> type(True)
+<class 'bool'>
+>>> 1<2
+True
+>>> 1>2
+False
+>>> 1==1
+True
+>>> 1!=1
+False
+>>> 1>=1
+True
+>>> 1<=1
+True
+>>> x=3
+>>> x*5
+15
+>>> "CiscoCiscoCisco"
+'CiscoCiscoCisco'
+>>> str1= "Cisco"
+>>> str2= "Networking"
+>>> str3="Academy"
+>>> space= " "
+>>> print(str1+space+str2+space+str3)
+Cisco Networking Academy
+>>> print(str1,str2,str3)
+Cisco Networking Academy
+>>> pi= 22/7
+>>> print(pi)
+3.142857142857143
+>>> print("{:.5f}".format(pi)
